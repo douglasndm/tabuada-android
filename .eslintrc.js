ea097895d82a364005eaacd723dddc4ba2f1ec91
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-08-03 03:08:32 -0300

    Upgrading React Native version and others depedencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-      es6: true,
+      es2020: true,
     },
     extends: [
       'plugin:react/recommended',
@@ -8,17 +8,11 @@ module.exports = {
       'prettier',
       'prettier/react'
     ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-      __DEV__: 'readonly'
-    },
-    parser: 'babel-eslint',
     parserOptions: {
       ecmaFeatures: {
         jsx: true,
       },
-      ecmaVersion: 2018,
+      ecmaVersion: 11,
       sourceType: 'module',
     },
     plugins: [
