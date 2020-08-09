2daed80c41f57fe18ffe21f724c04c32f5c4d15f
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-08-09 02:21:00 -0300

    Adding Sentry to the app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import * as Sentry from '@sentry/react-native';
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import EnvConfig from 'react-native-config';
 import { Provider } from 'react-native-paper';
 import { ThemeProvider } from 'styled-components';
 import './Services/Admob';
@@ -8,6 +10,10 @@ import './Services/Admob';
 import { GetTheme } from './Themes';
 
 import Routes from './routes';
+
+Sentry.init({
+    dsn: EnvConfig.SENTRY_DSN,
+});
 
 const theme = GetTheme();
 
