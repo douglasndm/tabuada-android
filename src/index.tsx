4b663e0a3a581b3b030d5a2df4496ec5318fe50e
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-12-16 19:22:23 -0300

    Removing Sentry and adding Crashlytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
-import * as Sentry from '@sentry/react-native';
 import 'react-native-gesture-handler';
 import React from 'react';
 import CodePush, { CodePushOptions } from 'react-native-code-push';
 import { NavigationContainer } from '@react-navigation/native';
-import EnvConfig from 'react-native-config';
 import { ThemeProvider } from 'styled-components/native';
 import './Services/Admob';
 
 import { GetTheme } from './Themes';
 
 import Routes from './routes';
-
-Sentry.init({
-    dsn: EnvConfig.SENTRY_DSN,
-});
 
 const theme = GetTheme();
 
